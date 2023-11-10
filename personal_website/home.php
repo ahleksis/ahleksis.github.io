ea02d131c87d1bb59fc6090e5133391588232d30
Kid <alexis.aclan@gmail.com>
2023-11-10 23:37:56 +0800

    personal website added

---
+++ personal_website/home.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title> Home Page</title>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<?php include 'navigationbar.php';  ?>
+
+<!-- BODY CONTENT -->
+<div class="content">
+<img class="prof" src="images/prof.jpg" alt="image" >
+<h1 class="name">Alexis Aclan </h1>
+<div class="info">
+	<p> a <i style="color: #FFFFFF; border-bottom: 2px solid #00FF00;">22</i>-year-old</p>
+	<p>lived in Baco, Oriental Mindoro</p>
+	<p>graduated at<i style="color: #FFFFFF; border-bottom: 2px solid #00FF00;"> Mindoro State University</i></p>
+      <!-- SOCIA; MEDIA -->
+  <div class="social-links"><center>
+    <a href="https://www.facebook.com/alexis.aclan.07" class="fa fa-facebook"></a>
+    <a href="https://www.instagram.com/alexis_aclan/" class="fa fa-instagram"></a>
+    <a href="https://www.gmail.com" class="fa fa-google"></a>
+    <a href="https://www.youtube.com/channel/UCyzxsH20Nf7MAHem25JL4lg/featured" class="fa fa-youtube"></a></center>
+  </div>
+</div>
+</div>
+</body>
+</html>
