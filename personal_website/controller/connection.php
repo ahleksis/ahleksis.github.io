e7aed063e463ae3ee9437dd1c470f6c85d126f85
Kid <alexis.aclan@gmail.com>
2023-11-10 23:51:20 +0800

    deleted

--- personal_website/controller/connection.php
+++
@@ -1,8 +0,0 @@
-<?php  
-$servername = "127.0.0.1";
-$username = "root";
-$password = "";
-$dbname = "myportfolio";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
